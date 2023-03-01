5285b69eb4c0c2dfe9178e014615486562f8a586
lukaszlewickii <lukasz.lewicki@edu.uekat.pl>
2023-03-01 13:18:27 +0100

    lab1

--- Laborki 1 - 22.02.23.py
+++ Laborki 1 - 22.02.23.py
@@ -3,16 +3,14 @@ import re
 # Zadanie 1. Znajdź wszystkie pozycje w tekście, gdzie znajdują się cyfry
 tekst = "Dzisiaj mamy 10 dzień miesiąca 03 roku 2011"
 
-#print(re.search('1', tekst))
+digits = "[0-9]"
 
-"""
-print("Original String : " + tekst) 
-num = ""
-for c in tekst:
-    if c.isdigit():
-        num = num + c
-print("Extracted numbers from the list : " + num) 
-"""
+#num = re.findall(digits, tekst) 
+#print(num)
+#print(re.search(digits,tekst))
 
-num = re.findall(r'\d', tekst) 
-print(num)
+re.search(digits, tekst).span()
+wynik = re.finditer(digits, tekst)
+
+for element in wynik:
+    element.span()[0]
